040d98ecbf8f9c228f2dde94ed530946fc5c9751
Nao <passmethepoke@gmail.com>
2018-07-21 18:48:18 +0900

    answerコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'answer/index'
+  get 'answer/new'
   get 'question/index'
   get 'question/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
